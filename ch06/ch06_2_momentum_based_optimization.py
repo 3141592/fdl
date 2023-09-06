942568855946d1ac1709e32a4a24027a33f4030b
R Gunter <roygunter@hushmail.com>
2023-09-05 22:22:27 -0400

    ch06

--- ch06/ch06_2_momentum_based_optimization.py
+++ ch06/ch06_2_momentum_based_optimization.py
@@ -52,7 +52,7 @@ lr = 1e-3
 loss_fn = nn.CrossEntropyLoss()
 epochs = 40
 classifier = BaseClassifier(in_dim, feature_dim, out_dim)
-optimizer = optim.SGD(classifier.parameters(), lr=lr)
+optimizer = optim.SGD(classifier.parameters(), lr=lr, momentum=0.9)
 
 def train(classifier=classifier,
           optimizer=optimizer,
@@ -105,114 +105,7 @@ def test(classifier=classifier,
             computed_loss.item()/len(test_loader)*64,
             accuracy*100.0/(len(test_loader)*64)))
 
-#train(classifier, optimizer, epochs, loss_fn)
+train(classifier, optimizer, epochs, loss_fn)
 
-#test(classifier, loss_fn)
-
-print("---------- ---------- ---------- ----------")
-print("Ch5. How Pesky Are Spurious Local Minima in Deep Networks?")
-
-class Net(nn.Module):
-  def __init__(self, in_dim, feature_dim, out_dim):
-    super(Net, self).__init__()
-    self.classifier = nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(in_dim, feature_dim),
-        nn.ReLU(),
-        nn.Linear(feature_dim, feature_dim),
-        nn.ReLU(),
-        nn.Linear(feature_dim, out_dim),
-        nn.ReLU()
-    )
-
-  def forward(self, inputs):
-    return self.classifier(inputs)
-
-print("Load checkpoint from SGD training")
-IN_DIM, FEATURE_DIM, OUT_DIM = 784, 256, 10
-model = Net(IN_DIM, FEATURE_DIM, OUT_DIM)
-
-# THIS DID NOT WORK
-#model.load_state_dict(torch.load("mnist.pt"))
-#print("model.parameters(): ", model.parameters)
-
-print("---------- ---------- ---------- ----------")
-print("Accesss the state dictionary from a model itself")
-import copy
-
-print("Access parameters with state_dict")
-print("Using a shallow copy")
-opt_state_dict = model.state_dict()
-
-for param_tensor in opt_state_dict:
-    print(param_tensor, "\t", opt_state_dict[param_tensor].size())
-
-print("")
-print("Using a deep copy")
-opt_state_dict = copy.deepcopy(model.state_dict())
-
-for param_tensor in opt_state_dict:
-    print(param_tensor, "\t", opt_state_dict[param_tensor].size())
-
-print("")
-print("Instantiate a new model with randomly initialized parameters")
-print("Create randonly initialized network")
-model_rand = Net(IN_DIM, FEATURE_DIM, OUT_DIM)
-rand_state_dict = copy.deepcopy(model_rand.state_dict())
-
-for param_tensor in rand_state_dict:
-    print(param_tensor, "\t", opt_state_dict[param_tensor].size())
-
-print("")
-print("Create a new state_dict for interpolated parameters")
-test_model = Net(IN_DIM, FEATURE_DIM, OUT_DIM)
-test_state_dict = copy.deepcopy(test_model.state_dict())
-
-alpha = 0.2
-beta = 1.0 - alpha
-for p in opt_state_dict:
-    test_state_dict[p] = (opt_state_dict[p] * beta +
-                          rand_state_dict[p] * alpha)
-
-print("")
-print("Compute the average loss over the entire test dataset using the model with the interpolated parameters")
-def inference(testloader, model, loss_fn):
-    running_loss = 0.0
-    with torch.no_grad():
-        for inputs, labels in test_loader:
-            outputs = model(inputs)
-            loss = loss_fn(outputs, labels)
-            running_loss += loss
-    running_loss /= len(testloader)
-    return running_loss
-
-print("")
-print("Vary the value of alpha to understand how the error surface changes")
-results = []
-for alpha in torch.arange(-2, 2, 0.05):
-    beta = 1.0 - alpha
-
-    #print("Compute interpolated parameters")
-    for p in opt_state_dict:
-        test_state_dict[p] = (opt_state_dict[p] * beta +
-                              rand_state_dict[p] * alpha)
-    #print("Load interpolated parameters into test model")
-    model.load_state_dict(test_state_dict)
-
-    #print("Compute loss given interpolated parameters")
-    # Typo in book had:
-    #loss = inference(trainloader, model, loss_fn)
-    loss = inference(train_loader, model, loss_fn)
-    results.append(loss.item())
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-plt.plot(np.arange(-2, 2, 0.05), results, 'ro')
-plt.ylabel('Incurred Error')
-plt.xlabel('Alpha')
-plt.show()
-
-
-
+test(classifier, loss_fn)
 
